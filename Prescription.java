49866250d0edc8e162bbf49134b842e2ccdc37b8
jwo227 <146120204+jwo227@users.noreply.github.com>
2025-10-20 16:59:18 -0400

    Add Prescription class with getters and setters
    Implemented Prescription class with fields and methods for managing prescription details.

---
+++ Prescription.java
@@ -0,0 +1,70 @@
+import java.util.*;
+
+public class Prescription {
+  private String id;
+  private String patientID;
+  private String patientName;
+  private String medication;
+  private String dosage;
+  private LocalDateTime issuedTime;
+  private boolean filled;
+
+  public Prescription(String id, String patientID, LocalDateTime issuedTime, String patientName, String medication, String dosage) {
+    this.id = id;
+    this.patientID = patientID;
+    this.issuedTime = issuedTime;
+    this.patientName = patientName;
+    this.medication = medication;
+    this.dosage = dosage;
+  }
+
+  public String getID() {return this.id;}
+
+  public String getPatientID() {
+    return this.patientID;
+  }
+  
+  public String getPatientName() {
+    return this.patientName;
+  }
+  
+  public String getMedication() {
+    return this.medication;
+  }
+  
+  public String getDosage() {
+    return this.dosage;
+  }
+
+  public LocalDateTime getIssuedTime() {
+    return this.issuedTime;
+  }
+
+  public void setID(String id) {
+    this.id = id;
+  }
+
+  public void setPatientID(String patientID) {
+    this.patientID = patientID;
+  }
+  
+  public void setPatientName(String patientName) {
+    this.patientName = patientName;
+  }
+  
+  public void setMedication(String medication) {
+    this.medication = medication;
+  }
+  
+  public void getDosage(String dosage) {
+    this.dosage = dosage;
+  }
+
+  public void setIssuedTime(LocalDateTime issuedTime) {
+    this.issuedTime = issuedTime;
+  }
+
+  public String toString() {
+    return String.format("", id);
+  }
+}
